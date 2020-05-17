eb6d98798a915e24882c38d3c98500db338035db
Swapnil <swapnil.khante@gmail.com>
2020-05-17 17:25:45 +0530

    Project setup added to git

---
+++ src/test/java/org/springphony/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springphony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
